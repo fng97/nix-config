174d6b86e323cd7e89356f009638ab4e7f8ccd62
Francisco <53615823+fng97@users.noreply.github.com>
2024-04-18 13:23:59 +0100

    - adds shell: zsh + alacritty + starship - adds extra config to git - adds back nvim - adds LazyVim deps - uses 'home.file' to link config

--- home.nix
+++ home.nix
@@ -6,19 +6,61 @@
 
   home.stateVersion = "23.11";
 
-  home.packages = [
-    # install stuff here
+  home.packages = with pkgs; [
+    # LazyVim
+    ripgrep
+    lazygit
+    fd
+    nodejs_21
   ];
+
+  programs.zsh.enable = true;
+
+  programs.alacritty.enable = true;
+  programs.alacritty.settings = {
+    window = {
+      padding.x = 18;
+      padding.y = 16;
+      decorations = "buttonless";
+    };
+
+    font = {
+      normal.family = "SFMono Nerd Font";
+      normal.style = "Regular";
+      size = 22;
+    };
+  };
+
+  programs.starship.enable = true;
+
+  home.file = {
+    ".config/nvim" = {
+      source = ./nvim;
+      recursive = true;
+    };
+  };
 
   home.sessionVariables = {
     EDITOR = "nvim";
   };
 
-  programs.home-manager.enable = true;
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+  };
 
   programs.git = {
     enable = true;
     userName = "fng97";
     userEmail = "53615823+fng97@users.noreply.github.com";
+    extraConfig = {
+      push.autoSetupRemote = "true";
+      init.defaultBranch = "main";
+    };
   };
+
+  programs.home-manager.enable = true;
 }
