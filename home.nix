64e07078ba624c6de1e25b46ac12e76b622da9d1
fng97 <53615823+fng97@users.noreply.github.com>
2024-06-16 16:10:50 +0100

    Set p10k theme.

--- home.nix
+++ home.nix
@@ -9,6 +9,7 @@
   home.packages = with pkgs; [
     wslu # for wslview
     gh
+    zsh-powerlevel10k
 
     # for LazyVim
     lazygit
@@ -38,13 +39,23 @@
   programs.zsh = {
     enable = true;
 
-    oh-my-zsh = {
-        enable = true;
-        theme = "robbyrussell";
-    };
+    oh-my-zsh.enable = true;
 
     syntaxHighlighting.enable = true;
     autosuggestion.enable = true;
+
+    plugins = [
+      {
+        name = "powerlevel10k";
+        src = pkgs.zsh-powerlevel10k;
+        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+      }
+      {
+        name = "powerlevel10k-config";
+        src = ./p10k;
+        file = "p10k.zsh";
+      }
+    ];
   };
 
   programs.neovim = {
