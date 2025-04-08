2b1c411c693fe303f9b4816bf993db2230b4ab5a
fng97 <53615823+fng97@users.noreply.github.com>
2025-04-08 07:04:47 +0000

    Scope let-in to WSL NixOS

--- flake.nix
+++ flake.nix
@@ -1,56 +1,50 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nixvim.url = "github:fng97/nixvim";
-    nixos-wsl = {
-      url = "github:nix-community/NixOS-WSL";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixos-wsl.url = "github:nix-community/NixOS-WSL";
+    nixos-wsl.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { nixpkgs, home-manager, nixos-wsl, ... }@inputs:
-    let
-      # pkgs is overridden in standalone home-manager (for macOS)
-      pkgs = nixpkgs.legacyPackages."x86_64-linux";
-    in {
-      nixosConfigurations."wsl" = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          nixos-wsl.nixosModules.default
-          {
-            system.stateVersion = "24.05";
-            nix.settings.experimental-features = [ "flakes nix-command" ];
-            wsl.enable = true;
-            wsl.defaultUser = "fng";
-            wsl.startMenuLaunchers = true;
-            users.defaultUserShell = pkgs.fish;
-            users.users.fng.extraGroups = [ "docker" ];
-            programs.fish.enable = true;
-            programs.nix-ld.enable = true;
-            virtualisation.docker.enable = true;
-          }
+  outputs = { nixpkgs, home-manager, nixos-wsl, ... }@inputs: {
 
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.fng = {
-              imports =
-                [ (import ./home.nix { inherit inputs pkgs; }) ./wsl.nix ];
-            };
-          }
-        ];
-      };
+    nixosConfigurations."wsl" = let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in nixpkgs.lib.nixosSystem {
+      inherit system;
+      modules = [
+        nixos-wsl.nixosModules.default
+        {
+          system.stateVersion = "24.05";
+          nix.settings.experimental-features = [ "flakes nix-command" ];
+          wsl.enable = true;
+          wsl.defaultUser = "fng";
+          wsl.startMenuLaunchers = true;
+          users.defaultUserShell = pkgs.fish;
+          users.users.fng.extraGroups = [ "docker" ];
+          programs.fish.enable = true;
+          programs.nix-ld.enable = true;
+          virtualisation.docker.enable = true;
+        }
 
-      # macOS
-      homeConfigurations.fng = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages."aarch64-darwin";
-        extraSpecialArgs = { inherit inputs; };
-        modules = [ ./home.nix ./macos.nix ];
-      };
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.extraSpecialArgs = { inherit inputs pkgs; };
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.fng = { imports = [ ./home.nix ./wsl.nix ]; };
+        }
+      ];
     };
+
+    # macOS
+    homeConfigurations.fng = home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages."aarch64-darwin";
+      extraSpecialArgs = { inherit inputs; };
+      modules = [ ./home.nix ./macos.nix ];
+    };
+  };
 }
