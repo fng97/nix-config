797930f76fc669535e82e97968a81dc6205ba363
fng97 <fng97@icloud.com>
2025-04-14 15:09:10 +0000

    wezterm: add keybinds for panes and tabs

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -19,10 +19,14 @@ function scheme_for_appearance(appearance)
 	end
 end
 
+local font = wezterm.font("JetBrains Mono")
+
 config.color_scheme = scheme_for_appearance(get_appearance())
-config.font = wezterm.font("JetBrains Mono")
-config.font_size = 15
+config.font = font
+config.font_size = 14
 config.hide_tab_bar_if_only_one_tab = true
+config.tab_max_width = 32
+config.use_fancy_tab_bar = false
 
 if wezterm.target_triple == "x86_64-pc-windows-msvc" and wezterm.running_under_wsl then
 	config.wsl_domains = {
@@ -36,5 +40,28 @@ if wezterm.target_triple == "x86_64-pc-windows-msvc" and wezterm.running_under_w
 else
 	config.default_prog = { "/etc/profiles/per-user/fng/bin/fish" }
 end
+
+config.leader = { key = "Space", mods = "CTRL", timeout_milliseconds = 1000 }
+
+config.keys = {
+	-- splits
+	{ key = "-", mods = "LEADER", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	{ key = "|", mods = "LEADER|SHIFT", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
+
+	-- new windows and tabs
+	{ key = "n", mods = "LEADER", action = wezterm.action.SpawnWindow },
+	{ key = "t", mods = "LEADER", action = wezterm.action.SpawnTab("DefaultDomain") },
+	{ key = "q", mods = "LEADER", action = wezterm.action.CloseCurrentPane({ confirm = false }) },
+
+	-- navigation between splits
+	{ key = "h", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Left") },
+	{ key = "j", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Down") },
+	{ key = "k", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Up") },
+	{ key = "l", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Right") },
+
+	-- navigation between tabs
+	{ key = "h", mods = "LEADER|CTRL", action = wezterm.action.ActivateTabRelative(-1) },
+	{ key = "l", mods = "LEADER|CTRL", action = wezterm.action.ActivateTabRelative(1) },
+}
 
 return config
