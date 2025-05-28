b0b034198d030f16b9714ac4c53190d5bdeaf0dd
fng97 <fng97@icloud.com>
2025-05-28 16:50:08 +0100

    WIP: wezterm modal navigation

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,32 +2,20 @@
 
 local wezterm = require("wezterm")
 
-local config = {}
+local config = wezterm.config_builder()
 
-function get_appearance()
-	if wezterm.gui then
-		return wezterm.gui.get_appearance()
-	end
-	return "Dark"
+config.font = wezterm.font("JetBrains Mono")
+config.font_size = 14
+config.window_decorations = "INTEGRATED_BUTTONS"
+
+-- Switch between light and dark themes based on system theme.
+if wezterm.gui.get_appearance():find("Dark") then
+	config.color_scheme = "Catppuccin Frappe"
+else
+	config.color_scheme = "Catppuccin Latte"
 end
 
-function scheme_for_appearance(appearance)
-	if appearance:find("Dark") then
-		return "Catppuccin Frappe"
-	else
-		return "Catppuccin Latte"
-	end
-end
-
-local font = wezterm.font("JetBrains Mono")
-
-config.color_scheme = scheme_for_appearance(get_appearance())
-config.font = font
-config.font_size = 15
-config.hide_tab_bar_if_only_one_tab = true
-config.tab_max_width = 32
-config.use_fancy_tab_bar = false
-
+-- On Windows, use NixOS-WSL and launch the fish shell.
 if wezterm.target_triple == "x86_64-pc-windows-msvc" and wezterm.running_under_wsl then
 	config.wsl_domains = {
 		{
@@ -41,27 +29,63 @@ else
 	config.default_prog = { "/etc/profiles/per-user/fng/bin/fish" }
 end
 
-config.leader = { key = "Space", mods = "CTRL", timeout_milliseconds = 1000 }
+-- Bind CTRL+Space to enter the "nav" key table (Zellij-like modal state). This lets us use neovim
+-- keybinds without clashing.
+
+local navigation_mode_active = false
+
+wezterm.on("update-right-status", function(window, pane)
+	if navigation_mode_active then
+		window:set_right_status(wezterm.format({
+			{ Attribute = { Intensity = "Bold" } },
+			{ Foreground = { AnsiColor = "Yellow" } },
+			{ Text = "   N   " },
+		}))
+	else
+		window:set_right_status("") -- clear
+	end
+end)
+
+wezterm.on("enter-navigation-mode", function()
+	navigation_mode_active = true
+end)
+
+wezterm.on("exit-navigation-mode", function()
+	navigation_mode_active = false
+end)
 
 config.keys = {
-	-- splits
-	{ key = "p", mods = "LEADER", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
-	{ key = "d", mods = "LEADER", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
+	{
+		key = "Space",
+		mods = "CTRL",
+		action = wezterm.action.Multiple({
+			wezterm.action.EmitEvent("enter-navigation-mode"),
+			wezterm.action.ActivateKeyTable({
+				name = "navigation",
+				one_shot = false, -- keep key table active until "PopKeyTable" is called
+			}),
+		}),
+	},
+}
 
-	-- new windows and tabs
-	{ key = "n", mods = "LEADER", action = wezterm.action.SpawnWindow },
-	{ key = "t", mods = "LEADER", action = wezterm.action.SpawnTab("DefaultDomain") },
-	{ key = "q", mods = "LEADER", action = wezterm.action.CloseCurrentPane({ confirm = false }) },
-
-	-- navigation between splits
-	{ key = "h", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Left") },
-	{ key = "j", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Down") },
-	{ key = "k", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Up") },
-	{ key = "l", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Right") },
-
-	-- navigation between tabs
-	{ key = "h", mods = "LEADER|CTRL", action = wezterm.action.ActivateTabRelative(-1) },
-	{ key = "l", mods = "LEADER|CTRL", action = wezterm.action.ActivateTabRelative(1) },
+config.key_tables = {
+	navigation = {
+		{ key = "h", action = wezterm.action.ActivatePaneDirection("Left") },
+		{ key = "j", action = wezterm.action.ActivatePaneDirection("Down") },
+		{ key = "k", action = wezterm.action.ActivatePaneDirection("Up") },
+		{ key = "l", action = wezterm.action.ActivatePaneDirection("Right") },
+		{ key = "H", action = wezterm.action.ActivateTabRelative(-1) },
+		{ key = "L", action = wezterm.action.ActivateTabRelative(1) },
+		{ key = "|", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+		{ key = "-", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
+		{
+			key = "Escape",
+			action = wezterm.action.Multiple({
+				wezterm.action.EmitEvent("exit-navigation-mode"),
+				"PopKeyTable",
+			}),
+		},
+	},
 }
 
 return config
