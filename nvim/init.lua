fcded024979bdaad7339166fa068db9faddc9860
fng97 <fng97@icloud.com>
2025-06-04 14:14:21 +0000

    nvim: Add git blame and remove redundant keymaps
    Some of these already have nvim defaults

--- nvim/init.lua
+++ nvim/init.lua
@@ -127,15 +127,10 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Page down and center" })
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Clear search highlight" })
 
 -- navigation
-vim.keymap.set("n", "<S-h>", "<cmd>bprevious<CR>", { desc = "Previous Buffer" })
-vim.keymap.set("n", "<S-l>", "<cmd>bnext<CR>", { desc = "Next Buffer" })
 vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Go to Left Window", remap = true })
 vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
 vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
 vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window", remap = true })
-vim.keymap.set("n", "<leader>e", function()
-	require("neo-tree.command").execute({ toggle = true, reveal = true })
-end, { desc = "UI: Toggle file [E]xplorer tree" })
 
 -- window management
 vim.keymap.set("n", "<leader>-", "<C-W>s", { desc = "Split Window Below", remap = true })
@@ -147,16 +142,18 @@ vim.keymap.set("n", "<C-Left>", "<cmd>vertical resize -2<CR>", { desc = "Decreas
 vim.keymap.set("n", "<C-Right>", "<cmd>vertical resize +2<CR>", { desc = "Increase Window Width" })
 
 -- buffer management
-vim.keymap.set("n", "<leader>nb", "<cmd>enew<CR>", { desc = "[N]ew [B]uffer" })
-vim.keymap.set("n", "<leader>db", "<cmd>bd<CR>", { desc = "[D]elete [B]uffer" })
-vim.keymap.set("n", "<leader>do", function()
+vim.keymap.set("n", "<S-h>", "<cmd>bprevious<CR>", { desc = "Previous Buffer" })
+vim.keymap.set("n", "<S-l>", "<cmd>bnext<CR>", { desc = "Next Buffer" })
+vim.keymap.set("n", "<leader>bn", "<cmd>enew<CR>", { desc = "[N]ew [B]uffer" })
+vim.keymap.set("n", "<leader>bd", "<cmd>bd<CR>", { desc = "[D]elete [B]uffer" })
+vim.keymap.set("n", "<leader>bo", function()
 	local current = vim.api.nvim_get_current_buf()
 	for _, buf in ipairs(vim.api.nvim_list_bufs()) do
 		if buf ~= current then
 			vim.api.nvim_buf_delete(buf, { force = true })
 		end
 	end
-end, { desc = "[D]elete [O]ther buffers" })
+end, { desc = "Delete [O]ther [B]uffers" })
 
 -- clipboard
 vim.keymap.set("n", "<leader>y", '"+y', { desc = "Yank to system clipboard" })
@@ -165,14 +162,15 @@ vim.keymap.set("n", "<leader>yy", '"+yy', { desc = "Yank line to system clipboar
 vim.keymap.set("v", "<leader>y", '"+y', { desc = "Yank selection to system clipboard" })
 vim.keymap.set("v", "<leader>p", '"+p', { desc = "Paste to selection from system clipboard" })
 
--- lsp goodies
-vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, { desc = "[C]ode: [R]ename symbol" })
-vim.keymap.set("n", "<leader>cd", vim.diagnostic.setloclist, { desc = "Open [C]ode [D]iagnostics" })
-vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Open [C]ode [A]ction" })
+-- IDE goodies
 vim.keymap.set("n", "gd", ts.lsp_definitions, { desc = "[G]oto [D]efinition" })
-vim.keymap.set("n", "gr", ts.lsp_references, { desc = "[G]oto [R]eferences" })
+vim.keymap.set("n", "<leader>gb", require("gitsigns").blame_line, { desc = "[G]it [B]lame" })
+vim.keymap.set("n", "<leader>gB", require("gitsigns").blame, { desc = "[G]it [B]lame (window)" })
 
 -- ui options
 vim.keymap.set("n", "<leader>ud", function()
 	vim.diagnostic.config({ virtual_text = not vim.diagnostic.config().virtual_text })
 end, { desc = "[U]I: Toggle [D]iagnostics" })
+vim.keymap.set("n", "<leader>e", function()
+	require("neo-tree.command").execute({ toggle = true, reveal = true })
+end, { desc = "UI: Toggle file [E]xplorer tree" })
