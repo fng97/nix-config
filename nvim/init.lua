831d61c1ec1c1a55bcd843714a9d7344887c237e
fng97 <fng97@icloud.com>
2025-07-25 16:37:05 +0100

    nvim: Configure lua_ls to find nvim
    Lets me use hover and go-to-definition in nvim config.

--- nvim/init.lua
+++ nvim/init.lua
@@ -71,7 +71,13 @@ vim.lsp.enable("lua_ls")
 vim.lsp.config("lua_ls", {
 	settings = {
 		Lua = {
+			runtime = { version = "LuaJIT" },
 			diagnostics = { globals = { "vim" } },
+			workspace = {
+				library = vim.api.nvim_get_runtime_file("", true),
+				checkThirdParty = false,
+			},
+			telemetry = { enable = false },
 		},
 	},
 })
