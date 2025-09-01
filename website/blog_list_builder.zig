5dcd82126364d6608fb12353b39eaa8ccfaa714e
fng97 <fng97@icloud.com>
2025-09-01 19:39:52 +0000

    website: Generate Atom feed

--- website/blog_list_builder.zig
+++ website/blog_list_builder.zig
@@ -12,6 +12,12 @@
 
 const std = @import("std");
 
+const Post = struct {
+    link_path: []const u8,
+    title: []const u8,
+    date: []const u8,
+};
+
 pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
@@ -19,32 +25,40 @@ pub fn main() !void {
 
     var args = try std.process.argsWithAllocator(allocator);
 
-    var index_path: ?[]const u8 = null;
-    var output_path: ?[]const u8 = null;
+    var index_in_path: ?[]const u8 = null;
+    var index_out_path: ?[]const u8 = null;
+    var feed_out_path: ?[]const u8 = null;
 
     _ = args.next(); // skip the first argument (the executable)
 
-    var blog_post_list_entries = std.ArrayList([]const u8).init(allocator);
+    var posts = std.ArrayList(Post).init(allocator);
 
     while (args.next()) |arg| {
-        const index_prefix = "--index=";
-        if (std.mem.startsWith(u8, arg, index_prefix)) {
-            std.debug.assert(index_path == null); // there can only be one
-            index_path = arg[index_prefix.len..];
+        const index_in_path_arg_prefix = "--index-in=";
+        if (std.mem.startsWith(u8, arg, index_in_path_arg_prefix)) {
+            std.debug.assert(index_in_path == null); // there can only be one
+            index_in_path = arg[index_in_path_arg_prefix.len..];
             continue;
         }
 
-        const output_prefix = "--output=";
-        if (std.mem.startsWith(u8, arg, output_prefix)) {
-            std.debug.assert(output_path == null); // there can only be one
-            output_path = arg[output_prefix.len..];
+        const index_out_path_arg_prefix = "--index-out=";
+        if (std.mem.startsWith(u8, arg, index_out_path_arg_prefix)) {
+            std.debug.assert(index_out_path == null); // there can only be one
+            index_out_path = arg[index_out_path_arg_prefix.len..];
             continue;
         }
 
-        const blog_post_prefix = "--blog-post=";
-        if (std.mem.startsWith(u8, arg, blog_post_prefix)) {
+        const feed_out_path_arg_prefix = "--feed=";
+        if (std.mem.startsWith(u8, arg, feed_out_path_arg_prefix)) {
+            std.debug.assert(feed_out_path == null); // there can only be one
+            feed_out_path = arg[feed_out_path_arg_prefix.len..];
+            continue;
+        }
+
+        const blog_post_arg_prefix = "--blog-post=";
+        if (std.mem.startsWith(u8, arg, blog_post_arg_prefix)) {
             // e.g. "--blog-post=20250705-i-like-coffee.html:{zig-cache-path}/stdout"
-            const arg_value = arg[blog_post_prefix.len..]; // strips --blog-post=
+            const arg_value = arg[blog_post_arg_prefix.len..]; // strips --blog-post=
             const colon_index = std.mem.indexOfScalar(u8, arg_value, ':').?;
             const link_path = arg_value[0..colon_index]; // 20250705-i-like-coffee.html
             const cache_path = arg_value[colon_index + 1 ..]; // {zig-cache-path}/stdout
@@ -55,44 +69,40 @@ pub fn main() !void {
             const content = try file.readToEndAlloc(allocator, file_stat.size);
             defer allocator.free(content);
 
-            // Parse the blog post title (between <title> and </title>).
+            // Find the blog post title (between <title> and </title>).
             const title_prefix = "<title>";
             const title_suffix = "</title>";
             const title_start = std.mem.indexOf(u8, content, title_prefix).? + title_prefix.len;
             const title_end = std.mem.indexOf(u8, content, title_suffix).?;
             const title = content[title_start..title_end];
 
-            // Parse the blog post publication date (between <p class="date"> and </p>).
+            // Find the blog post publication date (between <p class="date"> and </p>).
             const date_prefix = "<p class=\"date\">";
             const date_suffix = "</p>";
             const date_start = std.mem.indexOf(u8, content, date_prefix).? + date_prefix.len;
             const date_end = std.mem.indexOf(u8, content[date_start..], date_suffix).? + date_start;
             const date = content[date_start..date_end];
 
-            const list_entry = try std.fmt.allocPrint(
-                allocator,
-                "<li><a href=\"{s}\" target=\"_self\">{s}: {s}</a></li>",
-                .{ link_path, date, title },
-            );
-            try blog_post_list_entries.append(list_entry);
-
+            try posts.append(.{
+                .link_path = try allocator.dupe(u8, link_path),
+                .title = try allocator.dupe(u8, title),
+                .date = try allocator.dupe(u8, date),
+            });
             continue;
         }
 
         std.debug.panic("Unknown argument: {s}\n", .{arg});
     }
 
-    // To sort reverse-chronologically we can sort reverse-alphabetically because the blog list
-    // entries start the same until the part with the date (e.g. "<li><a href="20250705...").
-    std.mem.sort([]const u8, blog_post_list_entries.items, {}, reverse_alphabetical);
+    std.mem.sort(Post, posts.items, {}, reverse_chronological);
 
     // Read the input "index.html" file.
-    const index_in = try std.fs.cwd().readFileAlloc(allocator, index_path.?, 1024 * 1024);
+    const index_in = try std.fs.cwd().readFileAlloc(allocator, index_in_path.?, 1024 * 1024);
     defer allocator.free(index_in);
     const placeholder = "<!-- BLOG-POSTS -->";
     const placeholder_index = std.mem.indexOf(u8, index_in, placeholder).?;
 
-    const index_out = try std.fs.cwd().createFile(output_path.?, .{ .truncate = true });
+    const index_out = try std.fs.cwd().createFile(index_out_path.?, .{ .truncate = true });
     defer index_out.close();
     const index_out_writer = index_out.writer();
 
@@ -100,12 +110,50 @@ pub fn main() !void {
     // injecting the blog post list in between.
     try index_out_writer.writeAll(index_in[0..placeholder_index]);
     try index_out_writer.writeAll("<ul>\n"); // list start
-    for (blog_post_list_entries.items) |line| try index_out_writer.print("  {s}\n", .{line});
+    for (posts.items) |post| try index_out_writer.print(
+        "<li><a href=\"{s}\" target=\"_self\">{s}: {s}</a></li>",
+        .{ post.link_path, post.date, post.title },
+    );
     try index_out_writer.writeAll("</ul>\n"); // list end
     try index_out_writer.writeAll(index_in[placeholder_index + placeholder.len ..]);
+
+    var feed_out = try std.fs.cwd().createFile(feed_out_path.?, .{ .truncate = true });
+    defer feed_out.close();
+    const feed_out_writer = feed_out.writer();
+
+    try feed_out_writer.writeAll(
+        \\<?xml version="1.0" encoding="utf-8"?>
+        \\<feed xmlns="http://www.w3.org/2005/Atom">
+        \\  <title>Francisco's Blog</title>
+        \\  <author>
+        \\    <name>Francisco Nevitt Gonçalves</name>
+        \\  </author>
+        \\  <link href="https://francisco.wiki/"/>
+        \\  <link href="https://francisco.wiki/atom.xml" rel="self" type="application/rss+xml"/>
+        \\  <id>https://francisco.wiki/</id>
+    );
+
+    // Use most recent post's publication date for feed's last-updated.
+    try feed_out_writer.print("  <updated>{s}T00:00:00Z</updated>\n", .{posts.items[0].date});
+
+    for (posts.items) |p| {
+        try feed_out_writer.print(
+            \\  <entry>
+            \\    <title>{s}</title>
+            \\    <link href="https://francisco.wiki/{s}"/>
+            \\    <id>https://francisco.wiki/{s}</id>
+            \\    <updated>{s}T00:00:00Z</updated>
+            \\  </entry>
+            \\
+        , .{ p.title, p.link_path, p.link_path, p.date });
+    }
+
+    try feed_out_writer.writeAll("</feed>\n");
 }
 
-// Character values increase alphabetically. I.e. 'a' < 'b'.
-fn reverse_alphabetical(_: void, lhs: []const u8, rhs: []const u8) bool {
-    return std.mem.order(u8, lhs, rhs) == .gt;
+// To sort reverse-chronologically we can sort reverse-alphabetically because the blog list entries
+// start the same until the part with the date (e.g. "<li><a href="20250705..."). Character values
+// increase alphabetically. I.e. 'a' < 'b'.
+fn reverse_chronological(_: void, lhs: Post, rhs: Post) bool {
+    return std.mem.order(u8, lhs.date, rhs.date) == .gt;
 }
